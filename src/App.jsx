38d22a457406982d9dafa20ac04a5ba58e4bd2b5
Keshav Nagare <keshavnagare102@gmail.com>
2025-07-21 22:35:33 +0530

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <>
+      <h1>PowerPe</h1>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
